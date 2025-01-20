d539c19da731ba1fbd9081849d41bdd044fc197e
Jiyaro Joseph <jiyarojoseph2005@gmail.com>
2025-01-20 22:56:50 +0530

    updated_cpp

--- bubble-sort/bubbleSort.cpp
+++ bubble-sort/bubbleSort.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 
-// Bubble Sort function
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
+                // Swap arr[j] and arr[j+1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -15,24 +14,23 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-// Main function
-int main() {
-    int arr[] = {5, 7, 8, 222, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Original array: ";
+void printArray(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
+}
 
-    bubbleSort(arr, n); // Sorting the array
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Original array: ";
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
 
     cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+    printArray(arr, n);
 
     return 0;
 }
