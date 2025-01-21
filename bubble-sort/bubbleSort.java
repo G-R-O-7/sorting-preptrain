9d0ef418649d40b4f5461ea1a4ddf373431acf68
Jiyaro Joseph <jiyarojoseph2005@gmail.com>
2025-01-21 23:27:25 +0530

    new main

--- bubble-sort/bubbleSort.java
+++ bubble-sort/bubbleSort.java
@@ -1,11 +1,11 @@
-import java.util.Arrays; // Arrays utility for output
+import java.util.Arrays; // Imported for arrays manipulation
 
-public class BubbleSort { // this is 3rd line - BRANCH1
+public class BubbleSort { // This is 3rd line - MAIN BRANCH
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n; i++) { // Changed loop in branch1
+        for (int i = 0; i < n - 1; i++) { // Logic from main
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] < arr[j + 1]) { // Reversed comparison in branch1
+                if (arr[j] > arr[j + 1]) { // Main branch comparison
                     // Swap arr[j] and arr[j+1]
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
@@ -17,7 +17,7 @@ public class BubbleSort { // this is 3rd line - BRANCH1
 
     public static void main(String[] args) {
         int[] arr = {64, 34, 25, 12, 22, 11, 90};
-        System.out.println("Original array (BRANCH1): " + Arrays.toString(arr));
+        System.out.println("Original array (MAIN): " + Arrays.toString(arr));
         bubbleSort(arr);
         System.out.println("Sorted array: " + Arrays.toString(arr));
     }
